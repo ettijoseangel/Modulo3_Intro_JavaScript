f027cf6f126410ef88750b6897c3b5ac76ba1a8e
Angel <etti.joseangel@gmail.com>
2026-01-29 16:25:03 -0600

    Ejemplos resueltos de la primer práctica

---
+++ tipos-de-datos.js
@@ -0,0 +1,11 @@
+//Mensajes en consola (Práctica 1: Introducción a JavaScript)
+
+console.log(typeof 42); // number
+console.log(typeof "Veinticinco"); // string
+console.log(typeof -666); // number
+console.log(typeof true); // boolean
+console.log(typeof 0); // number
+console.log(typeof ''); // string
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+console.log(typeof FALSE); // undefined

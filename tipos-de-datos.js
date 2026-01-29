34f52d7bc330876c735b60840c3999c20779576a
Angel <etti.joseangel@gmail.com>
2026-01-29 16:27:19 -0600

    Ejemplos propios agregados al archivo

--- tipos-de-datos.js
+++ tipos-de-datos.js
@@ -9,3 +9,16 @@ console.log(typeof ''); // string
 console.log(typeof null); // object
 console.log(typeof undefined); // undefined
 console.log(typeof FALSE); // undefined
+
+//Ejemplos propios
+console.log(typeof NaN); // number
+console.log(typeof "true"); // string
+console.log(typeof 3.14); // number
+console.log(typeof [1, 2, 3]); // object
+console.log(typeof {nombre: "Ana", edad: 30}); // object
+console.log(typeof function() {}); // function
+console.log(typeof Symbol("id")); // symbol
+console.log(typeof BigInt(9007199254741991)); // bigint
+console.log(typeof new Date()); // object
+console.log(typeof /abc/); // object
+console.log(typeof Infinity); // number

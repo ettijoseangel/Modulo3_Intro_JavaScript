dea0bc587b63fef36307205f4e0074694e05cb2d
Angel <etti.joseangel@gmail.com>
2026-01-29 18:22:30 -0600

    Cambio de nombre del archivo

---
+++ 1_tipos-de-datos.js
@@ -0,0 +1,24 @@
+//Mensajes en consola (Práctica 1: Introducción a JavaScript)
+
+console.log(typeof 42); // number
+console.log(typeof "Veinticinco"); // string
+console.log(typeof -666); // number
+console.log(typeof true); // boolean
+console.log(typeof 0); // number
+console.log(typeof ''); // string
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+console.log(typeof FALSE); // undefined
+
+//Ejemplos propios
+console.log(typeof NaN); // number
+console.log(typeof "true"); // string
+console.log(typeof 3.14); // number
+console.log(typeof [1, 2, 3]); // object
+console.log(typeof {nombre: "Ana", edad: 30}); // object
+console.log(typeof function() {}); // function
+console.log(typeof Symbol("id")); // symbol
+console.log(typeof BigInt(9007199254741991)); // bigint
+console.log(typeof new Date()); // object
+console.log(typeof /abc/); // object
+console.log(typeof Infinity); // number
